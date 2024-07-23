6bcb617f3a4d2bf57fbb71c97e0b98d24ba94e96
CoderatureSheep <m49474@gmail.com>
2024-07-23 17:51:18 +0900

    확인

--- src/App.java
+++ src/App.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class App {
+import java.util.Scanner;
+
+
+public class App {
+    void run() {
+        System.out.println("프로그램 실행");
+        System.out.println("== 명언 앱 ==");
+
+        while(true) {
+            System.out.print("명령) ");
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            System.out.printf("입력하신 명령 : %s\n", cmd);
+
+            if (cmd.equals("종료")) break;
+            else if (cmd.equals("등록")) {
+
+                System.out.println("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+
+                System.out.printf("명언 : &s, 작가 : %s\n", content, authorName);
+
+            }
+        }
+
+    }
+
+//    void registration() {
+//        System.out.println("명언: ");
+//
+//        System.out.println("작가: ");
+//
+//    }
+
 }
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        new App().run();
+
+  //     int num = sc.nextInt();
+  //     System.out.println("입력받은 숫자 출력 : " + num);
+  //     System.out.printf("입력받은 숫자 출력할게요 : %d\n", num);
+
+
+
+    }
+}
